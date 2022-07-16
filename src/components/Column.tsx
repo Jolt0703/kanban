a527d2d863d5b68267fa105951bd5cf49ccd74c0
Matsuzaki Jotaro <midori1136@gmail.com>
2022-07-17 04:44:16 +0900

    Add: Drag-n-Drop functionality

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,79 +1,78 @@
-import React, { useEffect } from "react";
-import { ColumnInfo } from "../types";
+import React from "react";
+import { CardInfo, ColumnInfo } from "../types";
 import Cards from "./Cards";
 
-type ColumnProps = ColumnInfo & {
+export type ColumnProps = ColumnInfo & {
   addCard: (id: string, text: string, columnId: string) => void;
   setTitle: (id: string, title: string) => void;
+  swapCards: (fromCard: CardInfo, toCard: CardInfo) => void;
+  removeCard: (id: string) => void;
+  moveCardToColumn: (card: CardInfo, columnId: string) => void;
 };
 
-type ColumnTitleProps = Omit<ColumnProps, "cards" | "addCard">;
+type ColumnTitleProps = Omit<ColumnProps, "cards" | "addCard" | "swapCards" | "removeCard" | "moveCardToColumn">;
 
-// type ColumnTitleProps = Pick<ColumnProps, "title" | "setTitle">;
+const ColumnTitle: React.FC<ColumnTitleProps> = React.memo(({ id, title, setTitle }) => {
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [titleInput, setTitleInput] = React.useState(title);
+  const inputElement = React.useRef<HTMLInputElement>(null);
 
-const ColumnTitle: React.FC<ColumnTitleProps> = React.memo(
-  ({ id, title, setTitle }) => {
-    const [isEditing, setIsEditing] = React.useState(false);
-    const [titleInput, setTitleInput] = React.useState(title);
-    const inputElement = React.useRef<HTMLInputElement>(null);
+  React.useEffect(() => {
+    if (isEditing) {
+      inputElement.current!.focus();
+    }
+  }, [isEditing]);
 
-    useEffect(() => {
-      if (isEditing) {
-        inputElement.current!.focus();
-      }
-    }, [isEditing]);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleInput(e.target.value);
+  };
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setTitleInput(e.target.value);
-    };
+  const onClick = () => {
+    setIsEditing(true);
+  };
 
-    const onClick = () => {
-      setIsEditing(true);
-    };
+  const onBlur = () => {
+    const s = inputElement.current!.value.trim();
+    if (s) {
+      setTitle(id, s);
+      setIsEditing(false);
+    }
+  };
 
-    const onBlur = () => {
-      const s = inputElement.current!.value.trim();
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const s = e.currentTarget.value.trim();
       if (s) {
         setTitle(id, s);
         setIsEditing(false);
       }
-    };
+    }
+  };
 
-    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-      if (e.key === "Enter") {
-        const s = e.currentTarget.value.trim();
-        if (s) {
-          setTitle(id, s);
-          setIsEditing(false);
-        }
-      }
-    };
+  return (
+    <div className="column-title">
+      <h2 style={{ display: isEditing ? "none" : "block" }} onClick={onClick}>
+        {titleInput}
+      </h2>
+      <input
+        type="text"
+        value={titleInput}
+        onChange={onChange}
+        onBlur={onBlur}
+        onKeyPress={handleKeyPress}
+        ref={inputElement}
+        style={{ display: isEditing ? "block" : "none" }}
+      />
+    </div>
+  );
+});
 
-    return (
-      <div className="column-title">
-        <h2 style={{ display: isEditing ? "none" : "block" }} onClick={onClick}>
-          {titleInput}
-        </h2>
-        <input
-          type="text"
-          value={titleInput}
-          onChange={onChange}
-          onBlur={onBlur}
-          onKeyPress={handleKeyPress}
-          ref={inputElement}
-          style={{ display: isEditing ? "block" : "none" }}
-        />
-      </div>
-    );
-  }
-);
-
-const Column: React.VFC<ColumnProps> = React.memo(
-  ({ id, title, cards, setTitle, addCard }) => {
+const Column: React.FC<ColumnProps> = React.memo(
+  ({ id, title, cards, setTitle, addCard, swapCards, removeCard, moveCardToColumn }) => {
     return (
       <div className="column">
         <ColumnTitle id={id} title={title} setTitle={setTitle} />
-        <Cards columnId={id} cards={cards} addCard={addCard} />
+        <Cards columnId={id} cards={cards} addCard={addCard} swapCards={swapCards} removeCard={removeCard} />
       </div>
     );
   }
--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
-import { ColumnInfo } from "../types";
+import { CardInfo, ColumnInfo } from "../types";
 import Column from "./Column";
 
 type ColumnsProps = {
@@ -8,57 +8,108 @@ type ColumnsProps = {
   setColumns: React.Dispatch<React.SetStateAction<ColumnInfo[]>>;
 };
 
-const Columns: React.VFC<ColumnsProps> = React.memo(
-  ({ columns, setColumns }) => {
-    const onClick = (e: any) => {
-      const newColumn: ColumnInfo = {
-        id: uuidv4(),
-        title: `Column ${columns.length + 1}`,
-        cards: [],
-      };
-      setColumns([...columns, newColumn]);
+const Columns: React.FC<ColumnsProps> = React.memo(({ columns, setColumns }) => {
+  const onClick = (e: any) => {
+    const newColumn: ColumnInfo = {
+      id: uuidv4(),
+      title: `Column ${columns.length + 1}`,
+      cards: [],
     };
+    setColumns([...columns, newColumn]);
+  };
 
-    const addCard = (id: string, text: string, columnId: string) => {
-      setColumns(
-        columns.map((column) =>
-          column.id === columnId
-            ? {
-                id: columnId,
-                title: column.title,
-                cards: [...column.cards, { id, text, columnId }],
-              }
-            : column
-        )
-      );
-    };
-
-    const setColumnTitle = (id: string, title: string) => {
-      setColumns(
-        columns.map((column) =>
-          column.id === id ? { id, title, cards: column.cards } : column
-        )
-      );
-    };
-
-    return (
-      <div className="columns-container">
-        {columns.map((column) => (
-          <Column
-            key={column.id}
-            id={column.id}
-            title={column.title}
-            setTitle={setColumnTitle}
-            cards={column.cards}
-            addCard={addCard}
-          />
-        ))}
-        <button className="column add-column-btn" onClick={onClick}>
-          +
-        </button>
-      </div>
+  const addCard = (id: string, text: string, columnId: string) => {
+    setColumns(
+      columns.map((column) =>
+        column.id === columnId
+          ? {
+              id: columnId,
+              title: column.title,
+              cards: [...column.cards, { id, text, columnId }],
+            }
+          : column
+      )
     );
-  }
-);
+  };
+
+  const swapCards = (fromCard: CardInfo, toCard: CardInfo) => {
+    setColumns(
+      columns.map((column) => {
+        return {
+          id: column.id,
+          title: column.title,
+          cards: column.cards.map((card) => {
+            if (card.id === fromCard.id) {
+              return toCard;
+            }
+            if (card.id === toCard.id) {
+              return fromCard;
+            }
+            return card;
+          }),
+        };
+      })
+    );
+  };
+
+  const removeCard = (id: string) => {
+    setColumns(
+      columns.map((column) => {
+        return {
+          id: column.id,
+          title: column.title,
+          cards: column.cards.filter((card) => card.id !== id),
+        };
+      })
+    );
+  };
+
+  const moveCardToColumn = (card: CardInfo, columnId: string) => {
+    setColumns(
+      columns.map((column) => {
+        if (column.id === card.columnId) {
+          return {
+            id: column.id,
+            title: column.title,
+            cards: column.cards.filter((c) => c.id !== card.id),
+          };
+        }
+        if (column.id === columnId) {
+          return {
+            id: column.id,
+            title: column.title,
+            cards: [...column.cards, card],
+          };
+        }
+        return column;
+      })
+    );
+  };
+
+  const setColumnTitle = (id: string, title: string) => {
+    setColumns(columns.map((column) => (column.id === id ? { id, title, cards: column.cards } : column)));
+  };
+
+  return (
+    <div className="columns-container">
+      {columns.map((column) => (
+        <Column
+          key={column.id}
+          id={column.id}
+          title={column.title}
+          setTitle={setColumnTitle}
+          cards={column.cards}
+          addCard={addCard}
+          swapCards={swapCards}
+          removeCard={removeCard}
+          moveCardToColumn={moveCardToColumn}
+        />
+      ))}
+      <button className="column add-column-btn" onClick={onClick}>
+        +
+      </button>
+    </div>
+  );
+});
 
 export default Columns;
